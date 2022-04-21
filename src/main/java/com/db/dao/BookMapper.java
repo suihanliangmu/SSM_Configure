8c3f6a0f7c24f7122d2b5d24f546667316ab2728
DB <jan_7777777@163.com>
2022-04-21 20:57:07 +0800

    first commit

---
+++ src/main/java/com/db/dao/BookMapper.java
@@ -0,0 +1,28 @@
+package com.db.dao;
+
+import com.db.pojo.Books;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface BookMapper {
+
+    //增加一本书
+    int addBook(Books books);
+
+    //删除一本书
+    int deleteBookById(@Param("bookId") int id);
+
+    //更新一本书
+    int updateBook(Books books);
+
+    //查询一本书
+    Books queryBookById(@Param("bookID") int id);
+
+    //查询全部的书
+    List<Books> queryAllBook();
+
+    //搜索
+    Books queryBookByName(@Param("bookName") String bookName);
+
+}
---
+++ src/main/java/com/db/service/BookServiceImpl.java
@@ -0,0 +1,39 @@
+package com.db.service;
+
+import com.db.dao.BookMapper;
+import com.db.pojo.Books;
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService{
+
+    //service 调 dao 层：组合dao（把dao放进来）
+    private BookMapper bookMapper;
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    public int addBook(Books books) {
+        return bookMapper.addBook(books);
+    }
+
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    public List<Books> queryAllBook() {
+        return bookMapper.queryAllBook();
+    }
+
+    public Books queryBookByName(String bookName) {
+        return bookMapper.queryBookByName(bookName);
+    }
+}
